4ff4c726fe24962f9798ec6b2f70b8036a3b8113
marcel <marcel@strihovy>
2021-01-08 05:28:16 +0100

    test 1

---
+++ test_GIT/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: 8. 1. 2021
+ *      Author: marcel
+ */
+#include <avr/io.h>
+#include <stdlib.h>
+
+int main(void){
+	while(1){
+
+	}
+}
---
+++ test_GIT/main.h
@@ -0,0 +1,14 @@
+/*
+ * main.h
+ *
+ *  Created on: 8. 1. 2021
+ *      Author: marcel
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+
+#endif /* MAIN_H_ */
+
